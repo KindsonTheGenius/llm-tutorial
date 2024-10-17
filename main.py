59dea85f7075117a2c73197d58c5121a0ad77252
KindsonTheGenius <kindsonthegenius@gmail.com>
2024-10-17 15:58:06 +0000

    Added command line tool

--- main.py
+++ main.py
@@ -5,103 +5,115 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import click
 
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)
+# Create a click gorup
+@click.group()
+def cli():
+    """Pytorch beginner program ath allows you to change the batch size and epochs."""
 
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)
+@cli.command("train")
+@click.option("--batch-size", default=64, help="Batch size for training")
+@click.option("--epochs", default=5, help="Number of epochs")
+def train(batch_size: int, epochs: int):
+    """Train the Neural Network."""
+    print(f"Training with batch siz {batch_size} and {epochs} epochs")
 
-batch_size = 64
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
 
-# Crate the Data loaders
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
 
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y : {y.shape} {y.dtype}")
-    break
+    batch_size = batch_size
 
-# Get cpu or cuda if available
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
+    # Crate the Data loaders
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-# Define the model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-        )
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y : {y.shape} {y.dtype}")
+        break
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+    # Get cpu or cuda if available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
 
-model = NeuralNetwork().to(device)
-print(model)
+    # Define the model
+    class NeuralNetwork(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.flatten = nn.Flatten()
+            self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28 * 28, 512),
+                nn.ReLU(),
+                nn.Linear(512, 512),
+                nn.ReLU(),
+                nn.Linear(512, 10),
+            )
 
-# Create a loss function and an optimizer
-loss_function = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.linear_relu_stack(x)
+            return logits
 
+    model = NeuralNetwork().to(device)
+    print(model)
 
-# Train the model
-def train(dataloader, model, loss_function, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+    # Create a loss function and an optimizer
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
-        # compute the prediction error
-        pred = model(X)
-        loss = loss_function(pred, y)
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss {loss:>7f}, [{current:5d}/{size:>5d}]")
-
-# Test the model
-def test(dataloader, model, loss_function):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0,0
-    with torch.no_grad():
-        for X, y in dataloader:
+    # Train the model
+    def train(dataloader, model, loss_function, optimizer):
+        size = len(dataloader.dataset)
+        model.train()
+        for batch, (X, y) in enumerate(dataloader):
             X, y = X.to(device), y.to(device)
+
+            # compute the prediction error
             pred = model(X)
-            test_loss += loss_function(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
+            loss = loss_function(pred, y)
 
-    print(f"Test Loss: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(X)
+                print(f"loss {loss:>7f}, [{current:5d}/{size:>5d}]")
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}/{epochs}\n-------------------------------")
-    train(train_dataloader, model, loss_function, optimizer)
-    test(test_dataloader, model, loss_function)
-    print("Done!")
+    # Test the model
+    def test(dataloader, model, loss_function):
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        model.eval()
+        test_loss, correct = 0, 0
+        with torch.no_grad():
+            for X, y in dataloader:
+                X, y = X.to(device), y.to(device)
+                pred = model(X)
+                test_loss += loss_function(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test Loss: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    for t in range(epochs):
+        print(f"Epoch {t + 1}/{epochs}\n-------------------------------")
+        train(train_dataloader, model, loss_function, optimizer)
+        test(test_dataloader, model, loss_function)
+        print("Done!")
+
+if __name__ == "__main__":
+    cli()
